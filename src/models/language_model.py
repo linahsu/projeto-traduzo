7327c13fe3b28a6e42ae85602db02a68ced33a01
Lina Hsu <linahthsu@gmail.com>
2024-06-24 17:28:58 -0300

    feat: implements to_dict method in LanguageModel class

--- src/models/language_model.py
+++ src/models/language_model.py
@@ -6,3 +6,9 @@ class LanguageModel(AbstractModel):
 
   def __init__(self, data: dict) -> None:
     super().__init__(data)
+
+  def to_dict(self):
+    return {
+      "name": self.data["name"],
+      "acronym": self.data["acronym"],
+    }
