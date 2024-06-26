b21df6d79a6457635787743f222e08ec8f45588c
Lina Hsu <linahthsu@gmail.com>
2024-06-26 11:53:45 -0300

    feat: implements endpoint /history to return a list of translated history in JSON

--- src/controllers/history_controller.py
+++ src/controllers/history_controller.py
@@ -1,3 +1,10 @@
 from flask import Blueprint
+from models.history_model import HistoryModel
 
 history_controller = Blueprint("history_controller", __name__)
+
+
+@history_controller.route("/", methods=["GET"])
+def history():
+    translations = HistoryModel.list_as_json()
+    return translations, 200
